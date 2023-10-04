718394de97a5c1a86157215da3d5a08f0f1a16c5
Rushan <mukaefff@gmail.com>
2023-10-04 23:24:18 +0300

    Домашнее задание по теме Основные понятие выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
   
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d) ) / (2 * a), (-b - Math.sqrt(d) ) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  let percentMounthly = percent / 100 / 12; 
+  let credit = amount - contribution;
+
+  let payment = credit * (percentMounthly + (percentMounthly / ((Math.pow(1 + percentMounthly, countMonths)) - 1)));
+  let allPayments = payment * countMonths;
+
+  return +(allPayments.toFixed(2));
 }
